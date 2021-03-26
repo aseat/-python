a0a9950bed8621d98b7fbfc38d81aabddf11c2bf
aseat <73108756+aseat@users.noreply.github.com>
2021-03-26 22:33:33 +0900

    変数名、型表示

--- test.py
+++ test.py
@@ -1,2 +1,32 @@
-print("hello")
 print("good morning")
+print("hello")
+
+num = 1
+print(num)
+
+#01num = 7
+#print(01num)
+
+NUM = 2
+Num = 3
+print(NUM)
+print(Num)
+
+num01 = 123
+num02 = 1.23
+
+print(type(num01))
+print(type(num02))
+
+string_a = "Hello"
+
+print(string_a)
+print(type(string_a))
+
+a = 10
+b = 1
+
+bool01 = (a<b)
+
+print(bool01)
+print(type(bool01))
