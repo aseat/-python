31d25a34214894a3349760a653951a9b575b5cca
aseat <73108756+aseat@users.noreply.github.com>
2021-03-27 14:03:16 +0900

    演算子

--- test.py
+++ test.py
@@ -30,3 +30,79 @@ bool01 = (a<b)
 
 print(bool01)
 print(type(bool01))
+
+a= [["mari","kanan"],["chika","yosoro"]]
+
+
+print(a[0][0])
+print(a[0][1])
+print(a[1][0])
+print(a[1][1])
+
+x = 10
+y = 2
+z = 10
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x > y)
+print(x < y)
+print(x <= y)
+print(x >= z)
+print(x > y)
+print(x == y)
+print(x != y)
+
+x = 8
+y = 3
+print(x >= 5 and x <= 10)
+print(y >= 5 and y <=10)
+
+print(x == 3 or y == 3)
+print(x == 1 or y == 1)
+
+x = 8
+y = 12
+z = 20
+
+x +=10
+z += y
+print(x)
+print(z)
+
+age = 22
+
+if age >=20:
+    print("adult")
+
+age = 18
+
+if age >=20:
+  print("adult")
+else:
+    print("child")
+
+age = 0
+
+if age >= 20:
+  print("adult")
+elif age == 0:
+  print("baby")
+else:
+  print("child")
+
+for i in range(5):
+  print(i)
+
+for i in range(5):
+  if i ==3:
+    break
+  print(i)
+
+for i in range(5):
+  if i ==3:
+    continue
+  print(i)
