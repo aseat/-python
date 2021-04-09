ff7cbce825296d195630df45cc85c0a9130d42bd
aseat <73108756+aseat@users.noreply.github.com>
2021-04-09 20:06:16 +0900

    文字列抽出

--- middle.py
+++ middle.py
@@ -17,3 +17,12 @@ print(x, y) # 23 10
 x, y = 10, 23
 x, y = y, x
 print(x, y) # 23 10
+
+test = [1, 2, 3]
+
+print([test, 4, 5])  # [[1, 2, 3], 4, 5]
+print([*test, 4, 5])  # [1, 2, 3, 4, 5] 
+
+test = {'apple': 'red', 'remon': 'yellow'}
+
+print({**test,'cherrie': 'pink'}) # {'apple': 'red', 'remon': 'yellow', 'cherrie': 'pink'}
